1763466c44c156da21fb6fc3d215065729522942
ThurberDaniel <phxburg@gmail.com>
2021-06-22 11:49:42 -0600

    adding style to html

--- templates/server.py
+++ templates/server.py
@@ -1,1 +1,12 @@
-from flask import Flask, session, render, render_template, request
+from flask import Flask, session, render, render_template, request, redirect
+app = Flask(_name_)
+
+app.secret_key = "09h8rgtiywbecTHUNDERnihfgDANIELq7uyiwueb"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+app.run(debug=True)
